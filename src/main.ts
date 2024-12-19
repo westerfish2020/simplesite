60aed31db2b83ca02dbe5830c7c9047b3ba647d5
westerfish2020 <carolinehfwester@gmail.com>
2024-12-19 11:14:03 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
