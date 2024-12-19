23d8c278083c1b6151482aeac29c4580dc9f6c47
westerfish2020 <carolinehfwester@gmail.com>
2024-12-19 13:35:41 -0500

    fixed build issue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
